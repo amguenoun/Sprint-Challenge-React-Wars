a7a11528c356f74f3cd9009ca06cec8f17ff5aff
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-16 11:25:12 -0400

    added axios dependency and set up axios call to get array of people

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,14 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [peopleList, setPeopleList] = useState([]);
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/people/`)
+      .then(response => setPeopleList(response.data.results))
+      .catch(error => console.log("Error ", error))
+  }, [])
+
+  console.log(peopleList);
 
   return (
     <div className="App">

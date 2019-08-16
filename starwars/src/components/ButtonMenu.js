a8d5e659ee4415b39e9c2a5228d54ad2824c0802
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-16 13:28:12 -0400

    added a page number to the page

--- starwars/src/components/ButtonMenu.js
+++ starwars/src/components/ButtonMenu.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 
 const ButtonMenu = (props) => {
+    const [pageNumber, setPageNumber] = useState(1);
+
     const StyledDiv = styled.div`
         display:flex;
         justify-content:space-around;
         margin:2% 0%;
     `;
+
     return (
-        <StyledDiv>
-            <Button primary onClick={() => { props.linkHandler(props.data.previous) }}>Last Page</Button>
-            <Button primary onClick={() => { props.linkHandler(props.data.next) }}>Next Page</Button>
-        </StyledDiv>
+        <>
+            <h1 className="Header">Page {pageNumber}</h1>
+            <StyledDiv>
+                <Button primary onClick={() => { props.linkHandler(props.data.previous); setPageNumber(pageNumber - 1) }}>Last Page</Button>
+                <Button primary onClick={() => { props.linkHandler(props.data.next); setPageNumber(pageNumber + 1) }}>Next Page</Button>
+            </StyledDiv>
+        </>
     )
 }
 

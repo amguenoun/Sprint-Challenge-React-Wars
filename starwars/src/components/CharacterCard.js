7761a65e74abdac5e2c279548a37d217e27b0534
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-16 13:56:30 -0400

    added a react loader and implemented it in characterCards

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { Card, Icon } from 'semantic-ui-react'
 import styled from 'styled-components';
+import Loader from "./PageLoader";
 
 
 const CardExampleExtraContent = (props) => {
@@ -23,22 +24,27 @@ const CardExampleExtraContent = (props) => {
     const desc = `${props.name} is a ${props.height}cm and ${props.mass}kg ${species} from ${home}`;
 
     const StyledCard = styled(Card)`
-    width:290px;
-    height:180px;
-    margin:2% 0% !important;
+        width:290px;
+        height:180px;
+        margin:2% 0% !important;
        ${() => (props.gender === 'male' ? `background:#89cff0 !important` : null)};
        ${() => (props.gender === 'female' ? `background:#f4c2c2 !important` : null)};
     `;
 
+
     return (
-        <StyledCard>
-            <Card.Content header={props.name} />
-            <Card.Content description={desc} />
-            <Card.Content extra>
-                <Icon name='plane' />
-                {props.transport} Vehicles and Starships
-            </Card.Content>
-        </StyledCard>
+        <>
+            {(!species) ? (<Loader />) :
+                <StyledCard>
+                    <Card.Content header={props.name} />
+                    <Card.Content description={desc} />
+                    <Card.Content extra>
+                        <Icon name='plane' />
+                        {props.transport} Vehicles and Starships
+                </Card.Content>
+                </StyledCard>
+            }
+        </>
     )
 }
 
